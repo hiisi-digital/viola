77f52a826ac5b47db7acdf59a6b179df8ce08363
orgrinrt <ort@hiisi.digital>
2026-04-23 15:23:42 +0300

    feat: initial rust port scaffolding for viola core and abi

---
+++ target/debug/deps/viola_core-97d642d182b02891.d
@@ -0,0 +1,5 @@
+/Users/orgrinrt/Dev/clause-dev/viola/target/debug/deps/viola_core-97d642d182b02891.d: crates/viola-core/src/lib.rs
+
+/Users/orgrinrt/Dev/clause-dev/viola/target/debug/deps/libviola_core-97d642d182b02891.rmeta: crates/viola-core/src/lib.rs
+
+crates/viola-core/src/lib.rs:
---
+++ target/debug/deps/viola_core-e2f721d6c78e5293.d
@@ -0,0 +1,5 @@
+/Users/orgrinrt/Dev/clause-dev/viola/target/debug/deps/viola_core-e2f721d6c78e5293.d: crates/viola-core/src/lib.rs
+
+/Users/orgrinrt/Dev/clause-dev/viola/target/debug/deps/libviola_core-e2f721d6c78e5293.rmeta: crates/viola-core/src/lib.rs
+
+crates/viola-core/src/lib.rs:
